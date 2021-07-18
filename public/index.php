7ec3010918a1744e5c17175971ff88c3f950c71d
unknown <matheusmiranda39@gmail.com>
2021-07-18 10:07:25 -0300

    Tela de autenticação

--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
     require_once(dirname(__FILE__, 2) . '/src/config/config.php'); //o (,2) significa que irá pegar o PAI(INNOUT) do arquivo atual(public)
-    require_once(dirname(__FILE__, 2) . '/src/models/User.php'); //o (,2) significa que irá pegar o PAI(INNOUT) do arquivo atual(public)
-
-    $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-    print_r(User::get(['id' => 1], 'name, email'));
-    echo "<br>";
-    print_r(User::get(['name' => 'Chaves'], 'id, name, email'));
-    echo "<br>";
-    print_r(User::get(['name' => 'Chaves']));
-    echo "<br>";
-    print_r(User::get());
+    require_once(dirname(__FILE__, 2) . '/src/Views/Login.php');
 
 ?>
