6dd456d25b11d4b798094858a631315acdd886e8
unknown <matheusmiranda39@gmail.com>
2021-07-21 02:54:57 -0300

    Login correção

--- src/views/login.php
+++ src/views/login.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/comum.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/icofont.min.css">
     <link rel="stylesheet" href="assets/css/login.css">
-    <title>In 'N Out</title>
+    <title>In N' Out</title>
 </head>
 <body>
     <form class="form-login" action="#" method="post">
@@ -26,16 +26,17 @@
                     <label for="email">E-mail</label>
                     <input type="email" id="email" name="email"
                         class="form-control <?= $errors['email'] ? 'is-invalid' : '' ?>"
-                        value="<?= $email ?>" placeholder="Informe o e-mail" autofocus> 
+                        value="<?= $email ?>"
+                        placeholder="Informe o e-mail" autofocus>
                     <div class="invalid-feedback">
                         <?= $errors['email'] ?>
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="password">Senha</label>
-                    <input type="password" id="password" name="password" 
-                        class="form-control <?= $errors['password'] ? 'is-invalid' : '' ?>" 
-                        placeholder="Informe o senha">
+                    <input type="password" id="password" name="password"
+                        class="form-control <?= $errors['password'] ? 'is-invalid' : '' ?>"
+                        placeholder="Informe a senha">
                     <div class="invalid-feedback">
                         <?= $errors['password'] ?>
                     </div>
