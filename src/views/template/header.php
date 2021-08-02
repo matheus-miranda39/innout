2b92c91a10f013ee45c492dbcc94c09057967208
unknown <matheusmiranda39@gmail.com>
2021-08-02 18:48:07 -0300

    Header CSS

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -25,19 +25,18 @@
         <div class="spacer"></div>
         <div class="dropdown">
             <div class="dropdown-button">
-                <span>Usuario Mock</span>
-                <i class="icofont-simple-down ml-2"></i>
-            </div>
-            <div class="dropdown-content">
-                <ul class="nav-list">
-                    <li class="nav-item">
-                        <a href="logout.php"> 
-                            <i class="icofont-logout mr-2"></i>
-                            Sair
-                            
-                        </a>
-                    </li>
-                </ul>
+                <span class="ml-3">Usuario Mock</span>
+                <i class="icofont-simple-down mx-2"></i>
+                <div class="dropdown-content">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a href="logout.php"> 
+                                <i class="icofont-logout mr-2"></i>
+                                Sair
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </header>
